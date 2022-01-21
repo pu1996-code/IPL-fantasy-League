d368963c64a23d4f93faea449a1206928e01adac
pu1996-code <puspendraupraity00@gmail.com>
2022-01-21 14:25:50 +0530

    Updates

---
+++ precache-manifest.0443a6b294854ca17b45fbcbf43e5c0a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7161f705c5964d20d40ecfa3216c8d3b",
+    "url": "/IPL-fantasy-League/index.html"
+  },
+  {
+    "revision": "db44c1d36a22deabdc5b",
+    "url": "/IPL-fantasy-League/static/css/2.ba00281c.chunk.css"
+  },
+  {
+    "revision": "8a85ef7a022ecd38488c",
+    "url": "/IPL-fantasy-League/static/css/main.ff2cf452.chunk.css"
+  },
+  {
+    "revision": "db44c1d36a22deabdc5b",
+    "url": "/IPL-fantasy-League/static/js/2.849b4b60.chunk.js"
+  },
+  {
+    "revision": "e50910ddf4b24c995e30",
+    "url": "/IPL-fantasy-League/static/js/3.d2c8953f.chunk.js"
+  },
+  {
+    "revision": "8a85ef7a022ecd38488c",
+    "url": "/IPL-fantasy-League/static/js/main.6220f566.chunk.js"
+  },
+  {
+    "revision": "d163e2a6b2a701dfb9d5",
+    "url": "/IPL-fantasy-League/static/js/runtime~main.ac32de36.js"
+  },
+  {
+    "revision": "106ee74074021adcf17c397a52d045b0",
+    "url": "/IPL-fantasy-League/static/media/home_cover_image.106ee740.jpg"
+  }
+]);
